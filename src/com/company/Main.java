6fee3b67b88c7b0bf9084498581add1d3b5c71aa
alexeifilonenko <alexeifilonenko@gmail.com>
2018-11-23 17:38:55 +0300

    third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Employee theSecond = new Employee("Petr", "Petrov", 002, 600, "developer", "junior");
         Employee theThird = new Employee("Alex", "Sidorov", 003, 2200, "tester", "senior");
         Employee theFouth = new Employee("Bob", "Brown", 004, 3000, "developer", "lead");
+        Employee theFifth = new Employee("Jonh", "Fox", 005, 1300, "manager", "senior");
+        Employee theSixth = new Employee("Georg", "Wash", 006, 600, "junior", "tester");
 
     }
 }
